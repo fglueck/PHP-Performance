e5dd8a9ffdf730e74bef87ada12b3afad9836eb9
Frank Glück <glueck@dozent.net>
2021-02-15 09:34:41 +0100

    Create poperty_is_set.php

---
+++ poperty_is_set.php
@@ -0,0 +1,43 @@
+<?php
+error_reporting (0);
+class perf {
+    private $times = [];
+    private $start = null;
+
+    public function start() {
+        $this->start = microtime(true);
+    }
+    public function stop(string $label) {
+        $this->times[$label] = microtime(true) - $this->start;
+    }
+    public function report() {
+        asort($this->times, SORT_NUMERIC);
+        $bench = current($this->times);
+        foreach($this->times as $label => $time) {
+            $time = number_format($time, 5);
+            echo sprintf("%s%% %ss %s\n", number_format(bcmul(bcdiv($time,$bench,10),100, 10),1), $time, $label);
+        }
+    }
+}
+
+$p = new perf();
+$x = new class() { public $y = []; };
+
+$p->start();
+for($i=1000000;$i;$i--) {
+    if($x->y['']!=='');
+}
+$p->stop('No Check');
+    
+$p->start();
+for($i=1000000;$i;$i--) {
+    if(isset($x->y['']) and $x->y['']!=='');
+}
+$p->stop('isset()');
+
+$p->start();
+for($i=1000000;$i;$i--) {
+    if($x->y['']??''!=='');
+}
+$p->stop('??');
+$p->report();
